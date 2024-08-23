717b42339196d6198dae95dd85bb97ea70132f3f
notem <njm3308@rit.edu>
2024-08-23 17:25:57 -0400

    remove old file

--- samplestats.py
+++ samplestats.py
@@ -14,17 +14,22 @@ if __name__ == "__main__":
         data = pickle.load(fi)
     
     length_dist = []
+    times_dist = []
     if isinstance(data, dict):
         for key in data:
             for sample_list in data[key]:
                 for sample in sample_list:
                     length_dist.append(len(sample))
+                    times_dist.append(abs(sample[-1]))
     elif isinstance(data, list):
         for sample_list in key:
             for sample in sample_list:
                 length_dist.append(len(sample))
+                times_dist.append(abs(sample[-1]))
     
     percentiles = (50, 60, 70, 75, 80, 90, 95)
     statistics = numpy.percentile(length_dist, percentiles)
     print(percentiles)
+    print(statistics)
+    statistics = numpy.percentile(times_dist, percentiles)
     print(statistics)
